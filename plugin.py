8362f5329f1c588d3b7a5afd744eb8a78303e94d
fruitymang0 <53315403+fruitymang0@users.noreply.github.com>
2019-07-26 03:53:25 -0400

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -6,17 +6,19 @@ from supybot.commands import *
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 
+class Animals(callbacks.Plugin):
+    def doggo(self, irc, msg, args):
+        r = requests.get(url='https://random.dog/woof.json')
+        dogURL = r.json()['url']
+        irc.reply("%s: %s" % (msg.nick, dogURL))
+    doggo = wrap(doggo)
 
-def doggo(self, irc, msg, args):
-    r = requests.get(url='https://random.dog/woof.json')
-    dogURL = r.json()['url']
-    irc.reply("%s: %s" % (msg.nick, dogURL))
-doggo = wrap(doggo)
-
-def cat(self, irc, msg, args):
-    r = requests.get(url='https://aws.random.cat/meow')
-    catURL = r.json()['file']
-    irc.reply("%s: %s" % (msg.nick, catURL))
-cat = wrap(cat)
+    def cat(self, irc, msg, args):
+        r = requests.get(url='https://aws.random.cat/meow')
+        catURL = r.json()['file']
+        irc.reply("%s: %s" % (msg.nick, catURL))
+    cat = wrap(cat)
+    
+Class = Animals
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
