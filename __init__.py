f05bce2f65ccb900ed24c8022d6465d19644f948
fruitymang0 <53315403+fruitymang0@users.noreply.github.com>
2019-07-26 05:19:27 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ import supybot.world as world
 __version__ = ""
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
-__author__ = supybot.Author('Alice Smith', 'fruitymang0')
+__author__ = supybot.Author('Alice Smith', 'fruitymang0', '')
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
