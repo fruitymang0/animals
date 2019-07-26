8e0c3fcde83be2bc727a027a807b2b1ad6776817
fruitymang0 <53315403+fruitymang0@users.noreply.github.com>
2019-07-26 04:27:51 -0400

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,17 +1,11 @@
+import requests
+import string
+import supybot.conf as conf
 import supybot.utils as utils
 from supybot.commands import *
-import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
-try:
-    from supybot.i18n import PluginInternationalization
-    _ = PluginInternationalization('Animals')
-except:
-    # Placeholder that allows to run the plugin on a bot
-    # without the i18n module
-    _ = lambda x:x
-    
-    
+      
 class Animals(callbacks.Plugin):
     def doggo(self, irc, msg, args):
         r = requests.get(url='https://random.dog/woof.json')
@@ -24,7 +18,7 @@ class Animals(callbacks.Plugin):
         catURL = r.json()['file']
         irc.reply("%s: %s" % (msg.nick, catURL))
     cat = wrap(cat)
-
+    
     
 Class = Animals
 
