d7add647cd128202165ce407296d35c8614b2f3b
fruitymang0 <53315403+fruitymang0@users.noreply.github.com>
2019-07-26 05:18:59 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,27 +1,27 @@
 """
-This is a random animal image searcher.
+Pulls random animal pictures of the internet.
 """
 
 import supybot
 import supybot.world as world
-from imp import reload
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
 __version__ = ""
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
-__author__ = supybot.Author('Alice Smith', 'fruitymang0', '')
+__author__ = supybot.Author('Alice Smith', 'fruitymang0')
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {}
 
 # This is a url where the most recent plugin package can be downloaded.
-__url__ = 'https://github.com/fruitymang0/animals' 
+__url__ = '' # 'https://github.com/fruitymang0/animals'
 
 import config
 import plugin
+from imp import reload
 reload(plugin) # In case we're being reloaded.
 # Add more reloads here if you add third-party modules and want them to be
 # reloaded when this plugin is reloaded.  Don't forget to import them as well!
@@ -33,4 +33,4 @@ Class = plugin.Class
 configure = config.configure
 
 
-# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
